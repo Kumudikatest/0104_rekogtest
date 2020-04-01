fb2b9458cd015b965916e142067e89ed5ff4f019
HirudineeADRT <hirudinee@adroitlogic.com>
2020-04-01 17:19:26 +0530

    change queuname

--- 0104_rekogtest.js
+++ 0104_rekogtest.js
@@ -6,7 +6,7 @@ exports.handler = async (event) => {
 
     try {
         let data = await sqs.receiveMessage({
-            QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/hirutest0104`,
+            QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/hirutest01041`,
             MaxNumberOfMessages: 1,
             VisibilityTimeout: 30,
             WaitTimeSeconds: 0,
