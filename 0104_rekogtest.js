f627a1d4755290dfd7e85b80f36cc319701917df
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-10-12 13:42:38 +0530

    commit from gmail user

--- 0104_rekogtest.js
+++ 0104_rekogtest.js
@@ -1,34 +1,4 @@
-let AWS = require('aws-sdk');
-const s3 = new AWS.S3();
-const sqs = new AWS.SQS();
-const rekognition = new AWS.Rekognition();
-
 exports.handler = async (event) => {
-
-    try {
-        let data = await sqs.receiveMessage({
-            QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/hirutest01041`,
-            MaxNumberOfMessages: 1,
-            VisibilityTimeout: 30,
-            WaitTimeSeconds: 0,
-            AttributeNames: ['All']
-        }).promise();
-
-    } catch (err) {
-        // error handling goes here
-        //test ttt
-
-    };
-    try {
-        let data = await s3.listObjects({
-            Bucket: "induuuu",
-            MaxKeys: 10
-        }).promise();
-
-    } catch (err) {
-        // error handling goes here
-    };
-
-
-    return { "message": "Successfully executed" };
+    
+    return {"message": "Successfully executed"};
 };
